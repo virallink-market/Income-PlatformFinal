94e243b805cf3a0c73f174521cecc6b6971ae350
Viral Link <mdsultanmia5@gmail.com>
2025-10-04 05:33:48 +0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Termux/SWC এরর এড়াতে এই কনফিগারেশনটি ব্যবহার করা হয়েছিল।
+  swcMinify: false, 
+  
+  // ওয়ার্কস্পেস রুট ওয়ার্নিং ঠিক করার জন্য (Vercel এও সাহায্য করবে)
+  experimental: {
+    outputFileTracingRoot: __dirname, 
+  },
+};
+
+module.exports = nextConfig;
